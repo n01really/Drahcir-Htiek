bf283491896deff533f5f51771683e5440a2c442
Richard Trygg Svartling <tryggrichard@gmail.com>
2026-03-27 11:58:34 +0100

    Add initial entity class stubs in Entities namespace
    Created Drops, Furniture, Items, and NPC class files as empty internal classes within the Drahcir_Htiek.Entities namespace. Included standard .NET using directives in each file to prepare for future development.

---
+++ Entities/Furniture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drahcir_Htiek.Entities
+{
+    internal class Furniture
+    {
+    }
+}
